10516a457845d7db267120985cb53229a50c4353
naeemullah-datascientist <naeemullahmehmoodasghar@gmail.com>
2024-10-08 11:48:52 -0700

    Added smooth scroll functionality for navigation links.

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth Scroll for navigation links
+document.querySelectorAll('a.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
